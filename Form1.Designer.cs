36451bd083c4313308683b0dc164e1b0a1e95369
h8fuji <65300927+h8fuji@users.noreply.github.com>
2021-05-23 23:39:52 +0300

    j,yjdktybt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(82, 123);
+            this.button1.Location = new System.Drawing.Point(82, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(287, 84);
             this.button1.TabIndex = 0;
